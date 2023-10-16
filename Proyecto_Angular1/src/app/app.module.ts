fc5c22f058507dfdab87a1449859549ba953714f
Felipe694 <12754@utl.edu.mx>
2023-10-15 20:14:06 -0600

    Proyecto_Angular_Repositorio

--- Proyecto_Angular1/src/app/app.module.ts
+++ Proyecto_Angular1/src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule,Route, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+
+const appRoutes: Routes=[
+  {path:'inicio', component:InicioComponent},
+  {path:'nosotros', component:NosotrosComponent},
+  {path:'login', component:LoginComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatTabsModule,
+    MatMenuModule// Asegúrate de incluir el módulo aquí
   ],
   providers: [],
   bootstrap: [AppComponent]
