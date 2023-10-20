e8af575eb6c750b31d339a9cded48771c15a9547
Felipe694 <12754@utl.edu.mx>
2023-10-20 07:59:55 -0600

    Proyecto_Angular_Repositorio
    Proyecto_Angular_Repositorio

--- Proyecto_Angular1/src/app/app.module.ts
+++ Proyecto_Angular1/src/app/app.module.ts
@@ -13,13 +13,22 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatTabsModule} from '@angular/material/tabs';
 import { MatMenuModule } from '@angular/material/menu';
+import { MenuComponent } from './menu/menu.component';
+import { TablaComponent } from './tabla/tabla.component';
 
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+import {MatTableModule} from '@angular/material/table';
 
 const appRoutes: Routes=[
   {path:'inicio', component:InicioComponent},
   {path:'nosotros', component:NosotrosComponent},
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'menu', component:MenuComponent},
+  {path:'tabla', component:TablaComponent}
 ]
 
 
@@ -28,7 +37,9 @@ const appRoutes: Routes=[
     AppComponent,
     InicioComponent,
     NosotrosComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent,
+    TablaComponent
   ],
   imports: [
     RouterModule.forRoot(
@@ -42,7 +53,13 @@ const appRoutes: Routes=[
     MatToolbarModule,
     MatBadgeModule,
     MatTabsModule,
-    MatMenuModule// Asegúrate de incluir el módulo aquí
+    MatMenuModule,
+    
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    
+    MatTableModule// Asegúrate de incluir el módulo aquí
   ],
   providers: [],
   bootstrap: [AppComponent]
